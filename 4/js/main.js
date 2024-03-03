383c5313e5fbbddbecc3ce7dd1c80c4d7566425d
Keks <keks@htmlacademy.ru>
2024-03-04 01:02:07 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+import {createRandomObjects, createObject} from './mock-data';
+
+// console.log(createRandomObjects(25, createObject));
+
+// Special for ESLint
+createRandomObjects(25, createObject);
