197414389ad3d2adf7369867f5723a756d1dd2ba
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-03-17 13:34:48 +0300

    adds real data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-import {createRandomObjects, createObject} from './mock-data.js';
+// import {createRandomObjects, createObject} from './mock-data.js';
 import {renderThumbnails} from './thumbnails.js';
 import {setModalHandlers} from './big-picture/modal-controls.js';
 import {renderBigPicture} from './big-picture/big-picture.js';
 import {validateForm} from './form/form-validator.js';
+import {getData} from './api.js';
 import './form/form-controls.js';
 import './form/effect.js';
+import {findTemplate} from './utils/common.js';
 
-const randomObjects = createRandomObjects(25, createObject);
+const showGetDataErrorMessage = () => {
+  const message = findTemplate('data-error');
+  const body = document.querySelector('body');
+  body.append(message);
 
-renderThumbnails(randomObjects);
-setModalHandlers(renderBigPicture, randomObjects);
+  setTimeout(() => message.remove(), 5000);
+};
+
+getData()
+  .then((data) => {
+    renderThumbnails(data);
+    setModalHandlers(renderBigPicture, data);
+  })
+  .catch(showGetDataErrorMessage);
+
 validateForm();
