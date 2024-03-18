ad62e50558ad762c20822bb06343e3b76ad9993b
Keks <keks@htmlacademy.ru>
2024-03-19 01:17:41 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/utils/debounce.js
@@ -0,0 +1,13 @@
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export { debounce };
